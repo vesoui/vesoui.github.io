68dfa3909fdfdef7559296944a66890df0414916
Damian Ujma <devdmin@protonmail.ch>
2018-12-12 22:30:26 +0100

    v0.0.3

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ var App = angular.module('App', []);
 
 App.controller('MainCtrl', function ($scope, $filter, $http) {
   
-
+$scope.gender = null;
  $http.get("clothing.json").then(function(response) {
-     console.log("XD");
-        console.log(response.data);
+      $scope.clothing = response.data.clothing;
  });
+    
+    
+  $scope.genderFilter = function () {
+           
+            return function (item) {
+                console.log(item.gender + "  " + $scope.gender);
+                if (item.gender === $scope.gender) {
+                    return item;
+                }
+            };
+  }
+
+
+});
+
+
+App.controller('CollectionsCtrl', function ($scope, $filter, $http) {
+  
+$scope.gender = null;
+ $http.get("collections.json").then(function(response) {
+      $scope.clothing = response.data.collections;
+ });
+    
+    
+
 
 });
